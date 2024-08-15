504a6a2b6d3248913d96c975e7a88cc48614fad2
Murilo <mbalves1@outlook.com>
2024-08-15 15:06:28 -0300

    [feat]: init

---
+++ rest-api/src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common";
+import { CoursesModule } from "./courses/courses.module";
+
+@Module({
+  imports: [CoursesModule]
+})
+export class AppModule {
+
+}
---
+++ rest-api/src/courses/courses.module.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common";
+import { CoursesController } from "./controllers/courses.controller";
+
+@Module({
+  controllers: [CoursesController]
+})
+export class CoursesModule {
+
+}
